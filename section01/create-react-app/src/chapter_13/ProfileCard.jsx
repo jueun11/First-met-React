437a295a57cfc61983c8b889c3cd410e8a755334
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-17 18:22:26 +0900

    컴포넌트 조합

---
+++ section01/create-react-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="Jueun Kim" backgroundColor = "#4ea04e">
+      <p>안녕하세요,jueun11입니다</p>
+      <p>저는 리액트를 공부 중 입니다. 배가 고픕니다</p>
+      <p>생각해보니 피곤하기도합니다</p>
+    </Card>
+  );
+}
+
+export default ProfileCard
